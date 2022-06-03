c7f73f37227b2886621335da897be1fb61536381
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-06-03 15:01:35 +0930

    create heatLamp class

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,33 @@ class thermistor {
         return temp;
     }
 };
+class heatLamp {
+  public:
+    int lampPin;
+    heatLamp(int c) {
+        c = lampPin;
+        pinMode(lampPin, INPUT);
+    }
+    void on() {
+      digitalWrite(lampPin, HIGH);
+    }
+    void off() {
+      digitalWrite(lampPin, LOW);
+    }
+};
+thermistor thermistor1(A0);
+heatLamp lamp1(8);
+int currTemp;
 
 void setup() {
-  
+  Serial.begin(115200);
 }
 
 void loop() {
-  
+  currTemp = thermistor1.readTemp();
+  Serial.println(currTemp);
+  lamp1.on();
+  delay(1000);
+  lamp1.off();
+  delay(1000);
 }
